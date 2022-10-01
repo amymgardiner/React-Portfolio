370c564b3e6c64f5e45d7d1d92d838537a7ede82
Amy Gardiner <amymgardiner@gmail.com>
2022-10-01 12:16:34 -0500

    starting on footer component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import './index.css';
 
-import About from './components/About';
 import Header from './components/Header';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,7 +13,9 @@ function App() {
       <Header />
       <main>
         <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+
+function Footer() {
+  return (
+    <div className="footer-div">
+      <footer>
+        <ul>
+          <a href="https://github.com/amymgardiner" target="blank">
+            <li>
+              <FaGithub />
+            </li>
+          </a>
+          <a href="https://www.linkedin.com/in/amymgardiner/" target="blank">
+            <li>
+              <FaLinkedin />
+            </li>
+          </a>
+          <a href="https://www.instagram.com/amymgardiner/" target="blank">
+            <li>
+              <FaInstagram />
+            </li>
+          </a>
+        </ul>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
