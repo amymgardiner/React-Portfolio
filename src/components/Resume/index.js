a1321c339872a7ac46d7662c383b856585fb8d50
Amy Gardiner <amymgardiner@gmail.com>
2022-10-01 20:32:22 -0500

    ended up saving resume as image because it wouldn't render as a pdf

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Resume from '../../assets/Amy-Gardiner-Resume1024_1.png';
 
 function AmyResume() {
   return (
     <section class="about">
       <h1 id="about">Resume</h1>
+      <img src={Resume} alt="Amy Gardiner Resume" />
     </section>
   );
 }
