8a6fed91c436d7df5c054689bf2e200375785235
Amy Gardiner <amymgardiner@gmail.com>
2022-10-20 12:10:20 -0500

    made contact form work and send email with emailjs

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,86 +1,156 @@
-import React, { useState } from 'react';
-import { validateEmail } from '../../utils/helpers';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
+import emailjs from 'emailjs-com';
 
-function Contact() {
-  const [formState, setFormState] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+const Contact = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const [disabled, setDisabled] = useState(false);
 
-  const [errorMessage, setErrorMessage] = useState('');
-  const { name, email, message } = formState;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log('Submit Form', formState);
-    }
+  // Function that displays a success toast on bottom right of the page when form submission is successful
+  const toastifySuccess = () => {
+    toast('Form sent!', {
+      position: 'bottom-right',
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: false,
+      className: 'submit-feedback success',
+      toastId: 'notifyToast',
+    });
   };
 
-  const handleChange = (e) => {
-    if (e.target.name === 'email') {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage('your email is invalid.');
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log('Handle Form', formState);
+  // Function called on submit that uses emailjs to send email of valid contact form
+  const onSubmit = async (data) => {
+    // Destrcture data object
+    const { name, email, subject, message } = data;
+    try {
+      // Disable form while processing submission
+      setDisabled(true);
+
+      // Define template params
+      const templateParams = {
+        name,
+        email,
+        subject,
+        message,
+      };
+
+      // Use emailjs to email contact form data
+      await emailjs.send(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        templateParams,
+        process.env.REACT_APP_PUBLIC_KEY
+      );
+
+      // Reset contact form fields after submission
+      reset();
+      // Display success toast
+      toastifySuccess();
+      // Re-enable form submission
+      setDisabled(false);
+    } catch (e) {
+      console.log(e);
     }
   };
 
   return (
     <section>
       <h1 data-testid="h1tag">Contact Me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
+      <form id="contact-form" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="name">Full Name:</label>
           <input
             type="text"
             name="name"
-            defaultValue={name}
-            onBlur={handleChange}
-          />
+            {...register('name', {
+              required: {
+                value: true,
+                message: 'Please enter your name',
+              },
+              maxLength: {
+                value: 80,
+                message: 'Please use 30 characters or less',
+              },
+            })}
+            placeholder="Name"
+          ></input>
+          {errors.name && (
+            <span className="errorMessage">{errors.name.message}</span>
+          )}
         </div>
         <div>
           <label htmlFor="email">Email Address:</label>
           <input
             type="email"
             name="email"
-            defaultValue={email}
-            onBlur={handleChange}
-          />
+            {...register('email', {
+              required: true,
+              pattern:
+                /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+            })}
+            placeholder="Email address"
+          ></input>
+          {errors.email && (
+            <span className="errorMessage">
+              Please enter a valid email address
+            </span>
+          )}
+        </div>
+        <div>
+          <label htmlFor="name">Subject Line:</label>
+          <input
+            type="text"
+            name="subject"
+            {...register('subject', {
+              required: {
+                value: true,
+                message: 'Please enter a subject',
+              },
+              maxLength: {
+                value: 75,
+                message: 'Subject cannot exceed 75 characters',
+              },
+            })}
+            placeholder="Subject"
+          ></input>
+          {errors.subject && (
+            <span className="errorMessage">{errors.subject.message}</span>
+          )}
         </div>
         <div>
           <label htmlFor="message">Message:</label>
           <textarea
-            name="message"
             rows="5"
-            defaultValue={message}
-            onBlur={handleChange}
-          />
+            name="message"
+            {...register('message', {
+              required: true,
+            })}
+            placeholder="Message"
+          ></textarea>
+          {errors.message && (
+            <span className="errorMessage">Please enter a message</span>
+          )}
         </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button data-testid="button" type="submit">
+        <button
+          data-testid="button"
+          className="submit-btn"
+          disabled={disabled}
+          type="submit"
+        >
           Submit
         </button>
       </form>
+      <ToastContainer />
     </section>
   );
-}
+};
 
 export default Contact;
